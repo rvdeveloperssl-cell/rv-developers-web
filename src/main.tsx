9cc9c02c14e02206f529b9dbafbf9eabd0d9cb76
rvdeveloperssl-cell <rvdevelopers.sl@gmail.com>
2026-02-21 14:48:37 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* මෙතැනදී ඔයාගේ Client ID එක භාවිතා කරලා App එක Wrap කරනවා */}
+    <GoogleOAuthProvider clientId="966245396916-e104ocr1f6h277d59028h2n0hg644n8c.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
